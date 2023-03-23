3c8c951a5749ffe4291c3f5ef1a85bb1b4f97745
Zaynah <zaynah.sadiq@hotmail.com>
2023-03-23 17:46:19 +0000

    final changes

--- WhatShouldIWear.java
+++ WhatShouldIWear.java
@@ -12,17 +12,14 @@ public class WhatShouldIWear {
 
         if (temperature <=8 || weather.equals("snowing")){
             System.out.println("Wear a big coat!");
-        }else
+        } else if (temperature <=15 && weather.equals ("raining")) {
+            System.out.println("Wear a rain jacket");
+        } else if (temperature <=20 && weather.equals ("cloudy")){
+            System.out.println("Wear a jumper");
+        } else if (temperature >= 21 && weather.equals ("sunny")){
+            System.out.println("Wear some sunglasses!");
+        }
 
     }
 }
 
-
-
-//Create a program that makes suggestions on what to wear based on information it gathers about the weather. The purpose of this exercise is to give you some practise with logical and conditional operators.
-//
-//Allow your program to collect some descriptive user input about the weather ("rainy", "sunny", "cloudy", "snow", etc) and the average temperature for the day (8, 18, 28, etc).
-//
-//Let your program output clothing suggestions depending on a combination of weather conditions and temperature.
-//
-//e.g. if it is both "rainy" and less than 12 degrees, output "wear a waterproof winter coat"
